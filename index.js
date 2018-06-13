99c253a9f20dd988744e623ddceaa3c7d5e64bd2
Paul Jervis <jervis@12os.co.uk>
2018-06-13 20:36:46 +0100

    parse block

--- index.js
+++ index.js
@@ -6,39 +6,55 @@ function hash256(string) {
 };
 
 function blockString({
-                     index,
-                     minedBy,
-                     data,
-                     nonce,
-                     previousHash,
-                   }) {
+                       index,
+                       minedBy,
+                       data,
+                       nonce,
+                       previousHash,
+                     }) {
   return `${index} ${minedBy} ${data} ${previousHash} ${nonce}`;
 }
 
-const genesisBlock = {
+function parseBlock(blockString) {
+  if(blockString) {
+    let i = 2;
+    parts = blockString.split(' ');
+
+    return {
+      index: parts[i++],
+      minedBy: parts[i++],
+      data: parts[i++],
+      previousHash: parts[i++],
+      nonce: parts[i++],
+      hash: parts[i++],
+    };
+  }
+}
+
+const previousBlock = parseBlock(process.argv) || {
   index: 0,
-  minedBy: 'Genesis',
-  data: 'Genesis',
-  previousHash: '0',
-  nonce: 52458,
-  hash: '000021C1766F55BD5D413F0AC128A5D3D6B50E4F0D608B653209C4D468232C11',
+    minedBy:'Genesis',
+    data:'Genesis',
+    previousHash:'0',
+    nonce:52458,
+    hash:'000021C1766F55BD5D413F0AC128A5D3D6B50E4F0D608B653209C4D468232C11',
 };
 
 
 const newBlock = {
   index: 1,
-  minedBy: "Paul & Ed",
-  data: "123",
-  previousHash: genesisBlock.hash,
+  minedBy: 'Paul&Ed',
+  data: '123',
+  previousHash: previousBlock.hash,
   nonce: 0,
-}
+};
 
 let hash = hash256(blockString(newBlock));
 
-while(! hash.startsWith("0000")) {
+while(!hash.startsWith('0000')) {
   newBlock.nonce++;
   hash = hash256(blockString(newBlock));
 }
 
 console.log(hash);
-console.log("attempt: ", newBlock.nonce);
+console.log('attempt: ', newBlock.nonce);
