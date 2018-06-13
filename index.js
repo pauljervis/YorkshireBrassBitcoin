c61143c806a745efcc02132af81ea4397dd72e81
Paul Jervis <jervis@12os.co.uk>
2018-06-13 19:50:50 +0100

    add genesis block

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 var sha256 = require('sha-256-js');
 
 console.log(
-  sha256("abc")
+  sha256('abc')
 );
+
+const block = {
+  index: 0,
+  minedBy: 'Genesis',
+  data: 'Genesis',
+  previousHash: '0',
+  nonce: 52458,
+  hash: '000021C1766F55BD5D413F0AC128A5D3D6B50E4F0D608B653209C4D468232C11',
+};
+
+console.log(hashBlock(block));
+
+function hashBlock({
+                     index,
+                     minedBy,
+                     data,
+                     nonce,
+                   }) {
+  return `${index} ${data} ${minedBy} ${nonce}`;
+}
